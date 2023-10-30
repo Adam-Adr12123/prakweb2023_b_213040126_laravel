f6c351f6f2cb276eda65688e6e61c68b575a46df
adammason921@gmail.com <adammason921@gmail.com>
2023-10-30 23:17:13 +0700

    upload dari video laravel 3

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>WPU Blog | about</title>
+</head>
+<body>
+    <h1>Halaman about</h1>
+    <h3><?= $name ?></h3>
+    <p><?= $email ?></p>
+    <img src="img/<?= $img ?>" alt="Adam" width="200">
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Adam",
+        "email" => "Adam255@gmail.com",
+        "image" => "adr.png"
+    ]);
+});
+
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
