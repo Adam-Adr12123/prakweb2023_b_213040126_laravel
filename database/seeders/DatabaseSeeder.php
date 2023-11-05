adf40bbdee91ffb8c90ba78fe8cc8632a27ada87
adammason921@gmail.com <adammason921@gmail.com>
2023-11-05 20:46:49 +0700

    upload dari video laravel 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +22,56 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Post::create([
+            'name' => 'Adam',
+            'email' => 'adam555@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Peograming',
+            'slug' => 'web-programing',
+            
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+            
+        ]);
+
+        Post::create([
+            'name' => 'Judul 1',
+            'slug' => 'judul-1',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ullam nostrum, quisquam velit amet enim veritatis quo, ducimus delectus ea sequi deleniti sit?',
+            'category_id' => 1,
+            'category_id' => 1
+
+
+        ]);
+
+        Post::create([
+            'name' => 'Judul 2',
+            'slug' => 'judul-2',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ullam nostrum, quisquam velit amet enim veritatis quo, ducimus delectus ea sequi deleniti sit?',
+            'category_id' => 1,
+            'category_id' => 1
+
+
+        ]);
+        Post::create([
+            'name' => 'Judul 3',
+            'slug' => 'judul-3',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ullam nostrum, quisquam velit amet enim veritatis quo, ducimus delectus ea sequi deleniti sit?',
+            'category_id' => 2,
+            'category_id' => 1
+
+
+        ]);
     }
 }
+
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,14 +5,14 @@
     <article>
         <h1>{{ $post->title }}</h1>
 
-        <p>By. Adam in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/ $post->category->slug  }}">{{ $post->category->name }}</a></p>
         
        {!! $post->body !!}
 
 
     </article>
 
-    <a href="/blog">back to posts</a>
+    <a href="/blog" class="d-block mt-3">back to posts</a>
 @endsection
 
 
