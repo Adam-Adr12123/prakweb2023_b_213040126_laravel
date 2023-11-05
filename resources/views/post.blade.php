48b4e060e06af31755e7a4aeae9a606af04ca1d0
adammason921@gmail.com <adammason921@gmail.com>
2023-11-05 19:48:08 +0700

    upload dari video laravel 8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
 
     <article>
         <h1>{{ $post->title }}</h1>
+
+        <p>By. Adam in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         
        {!! $post->body !!}
 
@@ -12,3 +14,5 @@
 
     <a href="/blog">back to posts</a>
 @endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use PHPUnit\Framework\Attributes\PostCondition;
@@ -35,4 +36,20 @@ Route::get('/about', function () {
 Route::get('/blog', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
+Route::get('/categories', function()
+{
+    return view('category', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+        
+    ]);
+});
 
+Route::get('/categories/{Category:slug}', function(Category $category)
+{
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name,
+    ]);
+});
