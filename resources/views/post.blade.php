c17cbbb24c7c91ce1d8413419a5ffb6b5a7b7652
adammason921@gmail.com <adammason921@gmail.com>
2023-11-12 15:52:41 +0700

    upload dari video laravel 12

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,16 +3,30 @@
 @section('container')
 
     <article>
-        <h1>{{ $post->title }}</h1>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                <h1>{{ $post->title }}</h1>
 
-        <p>By. <a href="/authors/{{  $post->user->username }}</a> in <a href="/categories/ $post->category->slug  }}">{{ $post->category->name }}</a></p>
-        
-       {!! $post->body !!}
+                <p>By. <a href="/authors/{{  $post->author->username }}</a> in <a href="/categories/ $post->category->slug  }}">{{ $post->category->name }}</a></p>
+                <img src="https://source.unplash.com/1200x400/?{{ $posts-> category->name }}" alt="" class="img-fluid">
+                
+                <article class="">
+                {!! $post->body !!}
+                </article>
+                
+                
+                <a href="/blog" class="d-block mt-3">back to posts</a>
+
+
+                </div>
+            </div>
+        </div>
+      
 
 
     </article>
 
-    <a href="/blog" class="d-block mt-3">back to posts</a>
 @endsection
 
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,53 @@
 
 @section('container')
     <h1 class="mb-5">{{ $title }}</h1>
-    @foreach ($posts as $post)
+
+    @if ($posts->count() >)
+
+    <div class="card mb-3">
+        <img src="https://source.unplash.com/1200x400/?{{ $posts[0]-> category->name }}" class="card-img-top" alt="{{ $posts[0]-> category->name }}">
+        <div class="card-body">
+            <h3 class="card-title" class="text-decoration-none text-dark"   >{{ $posts[0]->title }}</h3>
+            <p>
+                <small class="text-muted">
+                By. <a href="/authors/{{ $post->author->username }} " class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/ {{$post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+               </small>
+            </p>
+
+            <p class="card-text"> {{ $posts[0]->excerpt }} </p>
+
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none btn btn-prima">Read More...</a>
+        </div>
+    </div>
+    @else
+        <p class="text-center fs-4">No post found.</p>
+    @endif
+
+    <div class="container">
+        <div class="row">
+            @foreach ($posts-skip(1) as $post)
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="position-absolute bg-dark p-3 text-white" style="background-color: rgba(0, 0, 0, 0.7)" ><a href="/posts/{{ $post->slug }}" class="text-white text-decoration-none"></a> {{ $post->category->name }} </div>
+                    <img src="https://source.unplash.com/1200x400/?{{ $posts-> category->name }}" class="card-img-top" alt="{{ $post-> category->name }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $post->title }}</h5>
+                        <p>
+                            <small class="text-muted">
+                                By. <a href="/authors/{{ $post->author->username }} " class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
+                            </small>
+                        </p>
+                        <p class="card-text">{{ $post->excerpt }}</p>
+                        <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            $endforeach
+        </div>
+
+    </div>
+
+    @foreach ($posts->skip(1) as $post)
         <article class="mb-5 pb-4 border-bottom">
             <h2>
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none" >{{ $post->title }}</a>
